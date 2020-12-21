1b8131dcf441a238bd0b50ba3d9220d91aa9e5b9
Shaun Limbeek <soccercomet@yahoo.com>
2020-12-21 15:16:23 -0800

    create the question and answer containers as well as the defining the timing variables to show on the main page

---
+++ script.js
@@ -0,0 +1,8 @@
+var timeShown = document.getElementById("#timeRemaining");
+var startTime = 100;
+var timeElapsed =0;
+
+timeLeft = startTime - timeElapsed;
+console.log(timeLeft);
+
+timeShown.textContent = timeLeft;
