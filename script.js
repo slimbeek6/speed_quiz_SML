d0b195a6f83da671fe007f5fc40a6517c017a243
Shaun Limbeek <soccercomet@yahoo.com>
2020-12-22 15:59:12 -0800

    Turned quesiton display into a function, made it dynamic based on TA input. Added display question to event listener for answers.

--- script.js
+++ script.js
@@ -1,63 +1,99 @@
 var timeShown = document.getElementById("#timeRemaining");
+var answerCheck = document.getElementById("right");
 var startBtn = document.querySelector("#startGame");
 var startDisplay = document.getElementById("#start-container")
 var questDisplay = document.getElementById("#question-container");
-var scoreDisplay = document.getElementById("score-container");
+var scoreDisplay = document.getElementById("#score-container");
 var currentQuestion = document.getElementById("#question");
 var finalScore = document.getElementById("finalScore");
 var initials = document.getElementById("initialsInput");
+var answerList = document.querySelector("#answerOptions");
 var answer1 = document.getElementById("answerOption1");
 var answer2 = document.getElementById("answerOption2");
 var answer3 = document.getElementById("answerOption3");
 var answer4 = document.getElementById("answerOption4");
-var startTime = 10;
+var startTime = 100;
 var questionCounter = 0;
 
 // Create list of questions
-var questionList = {
-    question1: {
+var questionList = [
+    {
         q: "Who made the number 7 shirt famous at Manchester United?",
         a1: "Wayne Rooney",
-        a2: "Cristiano Ronaldo",
+        a2: "David Beckham",
         a3: "Ruud van Nistelrooij",
-        a4: "George Best"
+        a4: "George Best",
+        ca: "4"
     },
-    question2: {
+    {
         q: "What year was Manchester United founded?",
         a1: "1878",
         a2: "1872",
         a3: "1921",
-        a4: "1888"
+        a4: "1888",
+        ca: "1"
+    },
+    {
+        q: "Who is the recordholder for goals scored for United?",
+        a1: "Cristiano Ronaldo",
+        a2: "Wayne Rooney",
+        a3: "Paul Ince",
+        a4: "Bobby Charlton",
+        ca: "2"
+    },
+    {
+        q: "How many first division titles does United have?",
+        a1: "15",
+        a2: "18",
+        a3: "20",
+        a4: "23",
+        ca: "3"
+    },
+    {
+        q: "What is the mascot of Manchester United?",
+        a1: "Gunners",
+        a2: "Red Devils",
+        a3: "Falcons",
+        a4: "Mean Machine",
+        ca: "2"
+    },
+    {
+        q: "Who is the current manager of United?",
+        a1: "Sir Alex Ferguson",
+        a2: "Wayne Rooney",
+        a3: "Jose Mourinho",
+        a4: "Ole Gunnar Solksjaer",
+        ca: "4"
     }
-};
+];
 
 
-console.log(questionList.question1);
-console.log(questionList.question2);
+var questionIndex = 0;
+var timeLeft = 0;
 
-currentQuestion.textContent = questionList.question1.q;
-answer1.textContent = questionList.question1.a1;
-answer2.textContent = questionList.question1.a2;
-answer3.textContent = questionList.question1.a3;
-answer4.textContent = questionList.question1.a4;
-
+// Define a function that displays the question and answer options, that is dynamic to choose questions and answers from a question array.
+function displayQuestion(){
+    currentQuestion.textContent = questionList[questionIndex].q;
+    answer1.textContent = questionList[questionIndex].a1;
+    answer2.textContent = questionList[questionIndex].a2;
+    answer3.textContent = questionList[questionIndex].a3;
+    answer4.textContent = questionList[questionIndex].a4;
+}
 
 // Create a function to contain and run the full quiz
 function startGame (event) {
     event.stopPropagation();
-    var timeLeft = startTime;
+    timeLeft = startTime;
     questDisplay.setAttribute("style", "text-align: center; display: float;");
     startDisplay.setAttribute("style", "text-align: center; display: none;");
 
-    // Start showing questions, need to create a for loop around this to run through each question until we reach the end of the number of questions.
-    for  (i = 1; i < questionList.length; i++){
-        var questionNum = "question"+i
-        currentQuestion.textContent = questionList.questionNum.q;
-        answer1.textContent = questionList.question1.a1;
-        answer2.textContent = questionList.question1.a2;
-        answer3.textContent = questionList.question1.a3;
-        answer4.textContent = questionList.question1.a4;
-    };
+    // Start showing questions
+        displayQuestion();
+        
+
+        // once answer chosen, return a right or wrong, and increment questionIndex, then re-print the questions and answers.
+        // add in an if statement that ends the game if the questionIndex equals questionList.length or timer is 0
+    
 
     // Create interval function to countdown the timer
     var timeInterval = setInterval(function() {
@@ -65,7 +101,7 @@ function startGame (event) {
         timeLeft--;
 
         // End game if time is 0
-        if (timeLeft === 0) {
+        if (timeLeft === 0 || questionIndex === questionList.length) {
             finalScore.textContent = timeLeft;
             clearInterval(timeInterval);
             questDisplay.setAttribute("style", "text-align: center; display: none;");
@@ -83,6 +119,18 @@ startBtn.addEventListener("click", startGame);
 
 // Create a listener to select an answer
 
+answerList.addEventListener("click", function(event){
+    var answer = event.target.value;
+    if (answer == questionList[questionIndex].ca) {
+        answerCheck.textContent = "Correct!";
+    }
+    else {
+        answerCheck.textContent = "Wrong!";
+        timeLeft = timeLeft - 10;
+    }
+    questionIndex++;
+    displayQuestion();
+});
 
 // Create a listener to submit the user's initials
 
@@ -90,6 +138,7 @@ initials.addEventListener("submit", function(event) {
     event.stopPropagation();
     localStorage.setItem("initials", initials.textContent);
     localStorage.setItem("score", timeLeft);
+    
 })
 
 // Create a function to populate the high scores list on the highscores page.
