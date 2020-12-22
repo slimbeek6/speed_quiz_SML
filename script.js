46b912a92522eef328ee499dffce248110c75c22
Shaun Limbeek <soccercomet@yahoo.com>
2020-12-21 21:35:19 -0800

    worked on javascript, connecting variables to the items on the html, and starting the interval function. Also created the button event listener, but it doesn't work yet

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 var timeShown = document.getElementById("#timeRemaining");
+var startBtn = document.getElementById("#startGame");
+var startDisplay = document.getElementById("#start-container")
+var questDisplay = document.getElementById("#question-container");
 var startTime = 100;
-var timeElapsed =0;
+var timeElapsed = 0;
 
-timeLeft = startTime - timeElapsed;
-console.log(timeLeft);
+// Create list of questions
+var question list = [
 
-timeShown.textContent = timeLeft;
+];
+
+
+// Create a function to contain and run the full quiz
+function startGame (event) {
+    event.stopPropagation();
+    var timeLeft = startTime;
+    questDisplay.setAttribute("style", "text-align: center; display: float;");
+    startDisplay.setAttribute("style", "text-align: center; display: none");
+
+    // Create interval function to countdown the timer
+    var timeInterval = setInterval(function() {
+        timeShown.textContent = timeLeft;
+        timeLeft--;
+
+        // End game if time is 0
+        if (timeLeft === 0){
+            
+        }
+    }, 1000);
+}
+
+
+// Create a listener to run the startGame function when the button is pressed
+
+startBtn.addEventListener("click", startGame);
+
+// Create a function to populate the high scores list on the highscores page.
